e9deba12a536825dceee1cc73e5a0a3b630290dd
kikuchan <kikuchan98@gmail.com>
2025-03-13 10:58:41 +0900

    string-reader: Change API and add JSDoc

--- packages/string-reader/main.ts
+++ packages/string-reader/main.ts
@@ -1,3 +1,6 @@
+function clamp(v: number, min: number, max: number) {
+  return Math.min(Math.max(v, min), max);
+}
 export class StringReader {
   #s: string;
   #pos: number = 0;
@@ -20,26 +23,59 @@ export class StringReader {
     return this.size - this.position;
   }
 
+  /**
+   * Whether the reader is at the end of the string
+   */
   eof() {
     return this.remain <= 0;
   }
 
+  /**
+   * Seek to a position
+   * @param n position to seek
+   * @returns this
+   */
   seek(n: number) {
-    this.#pos = n;
+    this.#pos = clamp(n, 0, this.size);
     return this;
   }
 
+  /**
+   * Skip n characters
+   * @param n number of characters to skip
+   * @returns this
+   */
   skip(n?: number) {
-    this.#pos += Math.min(n ?? 1, this.remain);
-    return this;
+    return this.seek(this.#pos + (n ?? 1));
   }
 
-  match(m: string | RegExp, cb?: (matched: string[]) => void) {
+  /**
+   * Match string or RegExp at current position
+   *
+   * This function does NOT advance the position
+   *
+   * @param m string or RegExp
+   * @returns matched strings, otherwise `false`
+   */
+  startsWith(m: string | RegExp) {
     if (this.eof()) return false;
 
     const sliced = this.#s.slice(this.position);
 
-    const matched = typeof m === 'string' ? sliced.startsWith(m) && [m] : sliced.match(new RegExp(m, 'y'));
+    return (typeof m === 'string' ? sliced.startsWith(m) && [m] : sliced.match(new RegExp(m, 'y'))) || false;
+  }
+
+  /**
+   * Match string or RegExp at current position
+   *
+   * This function advances the position if matched
+   *
+   * @param m string or RegExp
+   * @param cb callback function
+   * @returns matched strings, otherwise `false`
+   */
+  match(m: string | RegExp, cb?: (matched: string[]) => void) {
+    const matched = this.startsWith(m);
     if (!matched) return false;
 
     this.skip(matched[0].length);
@@ -47,29 +83,71 @@ export class StringReader {
     return matched;
   }
 
-  skipUntil(m: string | RegExp, cb?: (obj: { skipped: string; matched: string[] }) => void) {
-    if (this.eof()) return false;
-
+  #search<T>(m: string | RegExp, cb: (n: number) => T): T | false {
     const sliced = this.#s.slice(this.position);
+    const n = typeof m === 'string' ? sliced.indexOf(m) : sliced.search(m);
+    if (n < 0) return false;
 
-    const pos = typeof m === 'string' ? sliced.indexOf(m) : sliced.search(m);
-    if (pos < 0) return false;
-
-    const matched: string[] = typeof m === 'string' ? [m] : sliced.match(m)!;
-    const skipped = sliced.slice(0, pos);
-    this.skip(pos);
-    cb?.({ skipped, matched });
-    return { skipped, matched };
+    return cb(n);
   }
 
+  /**
+   * Search string or RegExp
+   *
+   * This function advances the position at the end of the matched string if found
+   *
+   * @param m string or RegExp
+   * @param cb callback function
+   * @returns matched information object, otherwise `false`
+   */
+  search(m: string | RegExp, cb?: (obj: { skipped: string; matched: string[] }) => void) {
+    return this.#search(m, (n) => {
+      const skipped = this.read(n);
+      const matched = this.match(m);
+      if (!matched) return false; // just in case
+      cb?.({ skipped, matched });
+      return { skipped, matched };
+    });
+  }
+
+  /**
+   * Skip until string or RegExp is found
+   *
+   * This function advances the position at the beginning of the matched string if found
+   *
+   * @param m string or RegExp
+   * @returns whther found or not
+   */
+  skipUntil(m: string | RegExp) {
+    return this.#search(m, (n) => this.skip(n)) && true;
+  }
+
+  /**
+   * Read until string or RegExp is found
+   *
+   * This function advances the position at the beginning of the matched string if found
+   *
+   * @param m string or RegExp
+   * @returns read string, `false` on not found
+   */
+  readUntil(m: string | RegExp) {
+    return this.#search(m, (n) => this.read(n));
+  }
+
+  /**
+   * Read n characters
+   *
+   * @param n number of characters to read
+   * @returns read string, '' on EOF
+   */
   read(n?: number) {
     if (this.eof()) return '';
 
     n = n ?? 1;
     if (n <= 0) n = this.remain + n;
 
-    const result = this.#s.slice(this.position, this.position + n);
+    const read = this.#s.slice(this.position, this.position + n);
     this.skip(n);
-    return result;
+    return read;
   }
 }
